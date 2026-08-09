b4a55730dfdce9c31bb53c3cd0395619851f19dc
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2026-08-08 20:06:39 -0700

    init python scanner

---
+++ crafting/python/scanner.py
@@ -0,0 +1,102 @@
+from tokens import Token
+
+
+class Scanner:
+    def __init__(self, source: str):
+        self.source = source
+        self.tokens: list[Token] = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+    def scan_tokens(self) -> list[Token]:
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_token()
+        self.tokens.apend(Token(TokenType.EOF, '', None, self.line))
+        return self.tokens
+
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
+
+    def scan_token():
+        char = self.advance()
+        match char:
+            case '(':
+                self.add_token(TokenType.LEFT_PAREN)
+            case ')':
+                self.add_token(TokenType.RIGHT_PAREN)
+            case '{':
+                self.add_token(TokenType.LEFT_BRACE)
+            case '{':
+                self.add_token(TokenType.RIGHT_BRACE)
+            case ',':
+                self.add_token(TokenType.COMMA)
+            case '.':
+                self.add_token(TokenType.DOT)
+            case '-':
+                self.add_token(TokenType.MINUS)
+            case '+':
+                self.add_token(TokenType.PLUS)
+            case ';':
+                self.add_token(TokenType.SEMICOLON)
+            case '*':
+                self.add_token(TokenType.STAR)
+            # case '!':
+            #     (self.add_token(TokenType.BANG_EQUAL) if self.match('=')
+            #      else self.add_token(TokenType.BANG))
+            # case '=':
+            #     (self.add_token(TokenType.EQUAL_EQUAL) if self.match('=')
+            #      else self.add_token(TokenType.EQUAL))
+            # case '<':
+            #     (self.add_token(TokenType.LESS_EQUAL) if self.match('=')
+            #      else self.add_token(TokenType.LESS))
+            # case '>':
+            #     (self.add_token(TokenType.GREATER_EQUAL) if self.match('=')
+            #      else self.add_token(TokenType.GREATER))
+            # case '/':
+            #     if self.match('/'):
+            #         while self.peek() != '\n' and not self.is_at_end():
+            #             self.advance()
+            #     else:
+            #         self.add_token(TokenType.SLASH)
+            # case ' ':
+            #     pass
+            # case '\r':
+            #     pass
+            # case '\t':
+            #     pass
+            # case '\n':
+            #     self.line += 1
+            # case '"' | "'":
+            #     self._string()
+            # case _:
+            #     if char.isdigit():
+            #         self._number()
+            #     elif char.isalpha():
+            #         self.identifier()
+            #     else:
+            #         ErrorHandler.error(self.line, 'Unexpected charcter')
+                    
+    def advance(self) -> str:
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def add_token(self, type: TokenType, literal: object = None) -> None:
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
+
+    def match():
+        pass
+
+    def peek():
+        pass
+
+    def _string():
+        pass
+
+    def _number():
+        pass
+
+    def identifier():
+        
